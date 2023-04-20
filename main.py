b3a57d37a2aa422de96e26cab8cc68b948bc455e
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2023-04-20 17:04:17 +0300

    Optimize and structurise

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import json
 from dotenv import dotenv_values
 import time
@@ -7,18 +6,20 @@ from datetime import datetime
 import telegram
 
 
-if __name__ == "__main__":
-    bot = telegram.Bot(dotenv_values(".env")["BOT_TOKEN"])
-    chat_id = dotenv_values(".env")["CHAT_ID"]
+def get_json_posts(file_name):
+    with open("posts.json", "r", encoding="UTF-8") as file:
+        posts_data = json.load(file)
+    return posts_data["posts"]
+
+
+def publish_posts_if_time(posts):
     while True:
-        with open("posts.json", "r", encoding="UTF-8") as file:
-            posts_data = json.load(file)
-        for post in posts_data["posts"]:
+        for post in posts:
             post_date = post["publication_date"]
             post_time = post["publication_time"]
 
-            currtime = datetime.strptime(str(datetime.now()), "%Y-%m-%d %H:%M:%S.%f")
-            post_datetime = datetime.strptime(post_date+" "+post_time, "%d.%m.%y %H:%M")
+            currtime = datetime.now()
+            post_datetime = datetime.strptime(f"{post_date} {post_time}", "%d.%m.%y %H:%M")
             if currtime > post_datetime and post["status"] == "not_publicated":
                 with open(post["image"], 'rb') as image:
                     bot.send_photo(
@@ -28,3 +29,10 @@ if __name__ == "__main__":
                     )
                     post["status"] = "publicated"
         time.sleep(10)
+
+
+if __name__ == "__main__":
+    bot = telegram.Bot(dotenv_values(".env")["BOT_TOKEN"])
+    chat_id = dotenv_values(".env")["CHAT_ID"]
+    posts = get_json_posts("posts.json")
+    publish_posts_if_time(posts)
